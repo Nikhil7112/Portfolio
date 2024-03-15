f2ff9fb4939426d0f2cb863cd48c0b126d691210
Nikhil Shukla <ns3060194@gmail.com>
2024-03-15 13:43:41 +0530

    initial comit

---
+++ react.js
@@ -0,0 +1,11 @@
+function openPage() {
+    window.open("react.html", "_blank");
+}
+
+let  openjsPage=()=>
+ {
+    window.open("js.html", "_blank");
+}
+document.querySelector('.back-button').addEventListener('click', () => {
+    window.open("index.html", "_blank");
+});
