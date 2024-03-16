22332a821e77ec363667b65dc4c4dd0f53c0e5ff
Nikhil Shukla <ns3060194@gmail.com>
2024-03-16 17:20:59 +0530

    changed

--- react.js
+++ react.js
@@ -9,3 +9,10 @@ let  openjsPage=()=>
 document.querySelector('.back-button').addEventListener('click', () => {
     window.open("index.html", "_blank");
 });
+const p1Img = document.getElementById("p1-img");
+
+p1Img.addEventListener("click", function() {
+   
+    const link = "https://ttgame711.netlify.app";
+    window.open(link, "_blank");
+});
